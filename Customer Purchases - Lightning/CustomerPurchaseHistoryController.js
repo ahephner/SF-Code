7a6d0561bb89259a3084c796f6b8378471920d01
ahephner <ajhephner178@gmail.com>
2018-10-25 16:48:59 -0400

    working for one customer

--- Customer Purchases - Lightning/CustomerPurchaseHistoryController.js
+++ Customer Purchases - Lightning/CustomerPurchaseHistoryController.js
@@ -1,10 +1,38 @@
 ({
-	getProductList : function(component, event, helper) {
-        var input = component.find('lookUp').get('v.value');
-        //console.log("before " + input);
-        //if i stringify it wont run through apex class
-        //var input = JSON.stringify(input);
-        //console.log(input);
-        helper.fetchProducts(component, input);
-	}
+	fetchProducts : function(component, event) {
+        var action = component.get("c.getProducts");
+        console.log(component.get("v.customer"));
+        
+            action.setParams({"custnum": component.get("v.customer")});
+         
+        action.setCallback(this, function(res){
+            var state = res.getState();
+            if(state === 'SUCCESS'){
+                console.log(state)
+                var call = res.getReturnValue(); 
+               console.log(call);
+                        //set atribute lengthn
+                        if(call.length == 0){
+                            component.set("v.message", true);
+                        }else{
+                            component.set("v.message", false);
+                        }
+                component.set("v.totals", call.length);
+                component.set("v.productList", call);
+}else if (state === "INCOMPLETE") {
+                alert('Response is Incompleted');
+            }else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        alert("Error message: " + 
+                                    errors[0].message);
+                    }
+                } else {
+                    alert("Unknown error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
+    },
 })
