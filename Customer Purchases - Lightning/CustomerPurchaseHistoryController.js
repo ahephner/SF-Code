00e2c4e83754ede687a5b6fbe7e61984479a6db0
ahephner <ajhephner178@gmail.com>
2018-10-25 16:51:01 -0400

    had components backward

--- Customer Purchases - Lightning/CustomerPurchaseHistoryController.js
+++ Customer Purchases - Lightning/CustomerPurchaseHistoryController.js
@@ -1,38 +1,17 @@
 ({
-	fetchProducts : function(component, event) {
-        var action = component.get("c.getProducts");
-        console.log(component.get("v.customer"));
-        
-            action.setParams({"custnum": component.get("v.customer")});
-         
-        action.setCallback(this, function(res){
-            var state = res.getState();
-            if(state === 'SUCCESS'){
-                console.log(state)
-                var call = res.getReturnValue(); 
-               console.log(call);
-                        //set atribute lengthn
-                        if(call.length == 0){
-                            component.set("v.message", true);
-                        }else{
-                            component.set("v.message", false);
-                        }
-                component.set("v.totals", call.length);
-                component.set("v.productList", call);
-}else if (state === "INCOMPLETE") {
-                alert('Response is Incompleted');
-            }else if (state === "ERROR") {
-                var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        alert("Error message: " + 
-                                    errors[0].message);
-                    }
-                } else {
-                    alert("Unknown error");
-                }
+        Search: function(component, event, helper) {
+            var input = component.find('lookUp');
+           //appears .focus() not working at this time there is a ticket in SF for it 
+           //  input.focus();
+            var isValueMissing = input.get('v.validity').valueMissing;
+            // if value is missing show error message and focus on field
+            if(isValueMissing) {
+                input.showHelpMessageIfInvalid();
+                input.focus();
+            }else{
+              // else call helper function 
+                helper.fetchProducts(component, event);
+                component.find("lookUp").set("v.value", '')
             }
-        });
-        $A.enqueueAction(action);
-    },
-})
+        },
+    })
